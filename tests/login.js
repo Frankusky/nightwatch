ac26642236db9a369247bd2f16a2771159f8e063
Frank Sam <sam.frank@autoweb.com>
2017-07-26 18:31:23 -0600

    Test adserver url

--- tests/login.js
+++ tests/login.js
@@ -1,11 +1,32 @@
 module.exports = {
-  'Login test': function (client) {
-    client
-      .url('http://google.com')
-//      .setValue('input[name="email"]', 'foo@bar.com')
-//      .setValue('input[name="password]', 'p455w0rdZ')
-//      .click('button[type="submit"]')
-//      .assert.containsText('main', 'News feed')
-      .end();
-  }
+	'Login test': function (client) {
+		client
+			.url('http://cdn.awadserver.com/templates/13/fas-track.html?make=Tesla&model=Model-X&zipcode=90210')
+			.perform(function (client, performDone) {
+				client.executeAsync(function (done) {
+					let tries = 0;
+					setInterval(function () {
+						performance.getEntriesByType("resource").find((el) => {
+							if (el.name.indexOf("Ads/Listings") > 0) {
+								console.log(el.name);
+								setTimeout(function () {
+									console.log("frank",client.assert)
+									done();
+
+								}, 1000)
+							} else {
+								if (tries === 5) {
+									console.log("test");
+									done()
+								}
+								tries++;
+							}
+						})
+					}, 1000);
+				}, [], function () {
+					performDone();
+				});
+			})
+//			.end();
+	}
 };
